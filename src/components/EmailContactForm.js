e76ca304ef916669538972e6ff8d8751eb626b14
ezraa <ezrakipchirchir1034@gmail.com>
2023-06-19 16:45:35 +0300

    emailjs functionality

--- src/components/EmailContactForm.js
+++ src/components/EmailContactForm.js
@@ -5,8 +5,8 @@ import Form from 'react-bootstrap/Form';
 const EmailContactForm = () => {
  const form = useRef();
 
- const sendEmail = (e) => {
-   e.preventDefault(); // prevents the page from reloading when you hit “Send”
+ const sendEmail = () => {
+    // prevents the page from reloading when you hit “Send”
 
    emailjs.sendForm('service_1q7do47', 'template_y4jbvu7', form.current, 'Az5A67-Qqt1GJKN4E')
      .then((result) => {
@@ -19,16 +19,19 @@ const EmailContactForm = () => {
  return (
 <form id="form" ref={form} onSubmit={sendEmail}>
   <div class="field">
-    <label for="user_name">user_name</label>
+    <label for="user_name">Name</label>
     <input type="text" name="user_name" id="user_name"/>
   </div>
-  <div class="field">
-    <label for="message">message</label>
-    <input type="text" name="message" id="message"/>
-  </div>
-  <div class="field">
-    <label for="user_email">user_email</label>
+  
+
+    <div class="field">
+    <label for="user_email">Email</label>
     <input type="text" name="user_email" id="user_email"/>
+  </div>
+
+  <div class="field">
+    <label for="message">Message</label>
+    < textarea name="message" id="message" rows={4}/>
   </div>
 
   <input type="submit" id="button" value="Send Email" />
